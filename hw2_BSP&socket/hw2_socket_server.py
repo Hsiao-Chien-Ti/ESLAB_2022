55bcdd08a1c3a8fe08262815c514e30692ba83f5
liukevin245 <74981089+liukevin245@users.noreply.github.com>
2022-10-09 11:57:37 +0800

    Add files via upload
    Add plotting code

--- hw2_BSP&socket/hw2_socket_server.py
+++ hw2_BSP&socket/hw2_socket_server.py
@@ -14,15 +14,51 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         while True:
             data = conn.recv(1024).decode('utf-8')
             print("Received from socket server:", data)
-            # if (data.count('{') != 1):
-            #     # Incomplete data are received.
-            #     choose = 0
-            #     buffer_data = data.split('}')
-            #     while buffer_data[choose][0] != '{':
-            #         choose += 1
-            #     data = buffer_data[choose] + '}'
-            # obj = json.loads(data)
-            # t = obj['s']
-            # plot.scatter(t, obj['x'], c='blue') # x, y, z, gx, gy, gz
-            # plot.xlabel("sample num")
-            # plot.pause(0.1)
+            if (data.count('{') != 1):
+                # Incomplete data are received.
+                choose = 0
+                buffer_data = data.split('}')
+                while buffer_data[choose][0] != '{':
+                    choose += 1
+                data = buffer_data[choose] + '}'
+                
+            obj = json.loads(data)
+            t = obj['s']
+            
+            plot.subplot(2, 3, 1)
+            plot.scatter(t, obj['x'], c='blue') # x, y, z, gx, gy, gz
+            plot.xlabel("sample num")
+            plot.ylabel("x")
+            plot.title("x vs sample num")
+            
+            plot.subplot(2, 3, 2)
+            plot.scatter(t, obj['y'], c='blue') # x, y, z, gx, gy, gz
+            plot.xlabel("sample num")
+            plot.ylabel("y")
+            plot.title("y vs sample num")
+            
+            plot.subplot(2, 3, 3)
+            plot.scatter(t, obj['z'], c='blue') # x, y, z, gx, gy, gz
+            plot.xlabel("sample num")
+            plot.ylabel("z")
+            plot.title("z vs sample num")
+            
+            plot.subplot(2, 3, 4)
+            plot.scatter(t, obj['gx'], c='blue') # x, y, z, gx, gy, gz
+            plot.xlabel("sample num")
+            plot.ylabel("gx")
+            plot.title("gx vs sample num")
+            
+            plot.subplot(2, 3, 5)
+            plot.scatter(t, obj['gy'], c='blue') # x, y, z, gx, gy, gz
+            plot.xlabel("sample num")
+            plot.ylabel("gy")
+            plot.title("gy vs sample num")
+            
+            plot.subplot(2, 3, 6)
+            plot.scatter(t, obj['gz'], c='blue') # x, y, z, gx, gy, gz
+            plot.xlabel("sample num")
+            plot.ylabel("gz")
+            plot.title("gz vs sample num")
+            
+            plot.pause(0.1)
